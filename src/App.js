6856b24a334ba2d9c211a18351b1ed38cddd5cf4
eduardo239 <eucrieiumaconta@gmail.com>
2025-05-31 12:27:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React from "react";
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+
+const App = () => {
+  return (
+    <Box>
+      <Card sx={{ maxWidth: 500, padding: 5, margin: 2 }}>
+        <CardMedia
+          component="img"
+          height="200"
+          image="https://www.lambdatest.com/blog/wp-content/uploads/2020/08/LambdaTest-320-180.png"
+          alt="LambdaTest"
+          sx={{
+            "@media screen and (max-width: 600px)": {
+              height: "150px",
+            },
+          }}
+        />
+        <CardContent
+          sx={{
+            "@media screen and (max-width: 600px)": {
+              paddingBottom: "20px",
+            },
+          }}
+        >
+          <Typography gutterBottom variant="h5" component="div">
+            LambdaTest{" "}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lambdatest is a cloud-based testing platform that allows developers
+            and testers to perform cross-browser testing of web applications. It
+            provides a virtual environment to test applications on a range of
+            browsers, operating systems, and devices.
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
 
 export default App;
