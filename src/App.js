d210588e0225eb06190a9267f9ad7bf77933058e
eduardo239 <eucrieiumaconta@gmail.com>
2025-05-31 12:35:14 -0300

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React from "react";
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 const App = () => {
   return (
-    <Box>
-      <Card sx={{ maxWidth: 500, padding: 5, margin: 2 }}>
-        <CardMedia
-          component="img"
-          height="200"
-          image="https://www.lambdatest.com/blog/wp-content/uploads/2020/08/LambdaTest-320-180.png"
-          alt="LambdaTest"
-          sx={{
-            "@media screen and (max-width: 600px)": {
-              height: "150px",
-            },
-          }}
-        />
-        <CardContent
-          sx={{
-            "@media screen and (max-width: 600px)": {
-              paddingBottom: "20px",
-            },
-          }}
-        >
-          <Typography gutterBottom variant="h5" component="div">
-            LambdaTest{" "}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lambdatest is a cloud-based testing platform that allows developers
-            and testers to perform cross-browser testing of web applications. It
-            provides a virtual environment to test applications on a range of
-            browsers, operating systems, and devices.
-          </Typography>
-        </CardContent>
-      </Card>
-    </Box>
+    <>
+     <Stack direction="horizontal" gap={2}>
+  <Button as="a" variant="primary">
+    Access
+  </Button>
+  <Button as="a" variant="success">
+    Generate
+  </Button>
+</Stack>;
+    </>
   );
 };
 
